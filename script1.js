5809686040fd9c50fd7b37d309ae58faa9ecaf14
Sammy <76911140+Sam-123-tech@users.noreply.github.com>
2021-02-01 23:29:35 -0800

    Add files via upload

---
+++ script1.js
@@ -0,0 +1,11 @@
+var a=prompt("Enter user name");
+ var b=prompt("Enter password");
+  	if(a== "Cortana"&&b=="Gulu")
+  	{
+  	    window.location.href="start1.html";
+  	} 
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="Index10.html";
+  	} 
